1a2dc24918397d0d91a1d4c7da568d21be84d993
Solitude <xczcdjx@126.com>
2026-05-13 18:07:59 +0800

    compose respect

---
+++ app/src/main/java/com/djx/composerespect/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.djx.composerespect
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import com.djx.composerespect.components.TextCom
+import com.djx.composerespect.ui.theme.ComposeRespectTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ComposeRespectTheme {
+                MainScreen()
+            }
+        }
+    }
+}
+
+@Composable
+fun MainScreen() {
+    Scaffold(
+        modifier = Modifier.fillMaxSize()
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ) {
+            TextCom()
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true, device = Devices.PIXEL_2, group = "Group2")
+@Composable
+fun GreetingPreview() {
+    MainScreen()
+}
---
+++ app/src/main/java/com/djx/composerespect/components/TextCom.kt
@@ -0,0 +1,9 @@
+package com.djx.composerespect.components
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun TextCom(){
+    Text("111123")
+}
